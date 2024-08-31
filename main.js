cb54c113bff4ce5c9ff338821db36f512e8beaa1
Abhishekkannaujiya <abhishekkannaujiya824@gmail.com>
2024-08-31 13:24:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let word = "Abhishek"; let numTimes = 5;
+
+console.log(word.repeat(numTimes));
+
+console.log(word.repeat(numTimes));
+    var typed = new Typed('#element', {
+      strings: ['software developer','web developer','machine learning','Game developer.'],
+      typeSpeed: 50,
+    });
+ 
+//  const prevEle = document.querySelector(".prev")
+  //const nextEle = document.querySelector(".next")
+//  const imgContainerEle = document.querySelector(".img-container")
+ // let currimg = 1;
+  
+ // nextEle.addEventListener("click",()=>{currimg++
+  
+  //updateImg()})
+  
+ // function updateImg(){
+   // imgContainerEle.style.transform = //'translatex(${-(currimg - 1) * 100}%)'
+    
+//  }
+  
